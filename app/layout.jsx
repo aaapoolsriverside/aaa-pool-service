ae650b547b8404fac82507ac7c50bce09dd6552d
aaapoolsriverside <aaapools85@icloud.com>
2026-01-25 22:06:31 -0800

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>AAA Pool Service & Repair</h1>
+      <p>Professional Pool Cleaning & Repairs in Riverside, CA</p>
+      <p>📞 Call: (951) 396-9765</p>
+      <p>📧 Email: aaapools85@icloud.com</p>
+    </div>
+  );
+}
